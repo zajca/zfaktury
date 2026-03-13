0e3b6df7c3e74521a85afd71f4b4723da137b71b
Martin Zajíc <ja@mzajic.cz>
2026-03-13 08:17:54 +0100

    Move Fakturoid import from main nav to Settings section

--- frontend/src/lib/components/Layout.svelte
+++ frontend/src/lib/components/Layout.svelte
@@ -75,11 +75,6 @@
 			]
 		},
 		{
-			href: '/import/fakturoid',
-			label: 'Import',
-			icon: 'M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12'
-		},
-		{
 			href: '/contacts',
 			label: 'Kontakty',
 			icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
@@ -139,6 +134,11 @@
 					href: '/settings/categories',
 					label: 'Kategorie',
 					icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A2 2 0 013 12V7a4 4 0 014-4z'
+				},
+				{
+					href: '/import/fakturoid',
+					label: 'Import z Fakturoid',
+					icon: 'M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12'
 				}
 			]
 		}
